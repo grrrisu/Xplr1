0dac4864ed010a7427c1706911be47929672597a
Alessandro Di Maria <adm@m42.ch>
2025-07-21 11:32:26 +0200

    format markdown as html

--- mix.exs
+++ mix.exs
@@ -59,6 +59,7 @@ defmodule Xplr1.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
+      {:mdex, "~> 0.7"},
       {:dotenv, "~> 3.0", runtime: Mix.env() == :dev}
     ]
   end
